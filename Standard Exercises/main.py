34d76b54a37a7582c5beb6902eadcd83b2fc71fc
cfittro <cdfittro@gmail.com>
2019-03-19 17:05:48 -0700

    exercises 6-10

--- Standard Exercises/main.py
+++ Standard Exercises/main.py
@@ -1,6 +1,7 @@
 import sys
 import datetime as dt
 import math
+import os
 
 #1: Write a Python program to print the following string in a specific format (see the output).
 print('Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are')
@@ -31,3 +32,40 @@ def name_reverse ():
 #name_reverse()
 
 #6: Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+def comma_separated ():
+    data = input("Please enter any number of comma separated values: ")
+    list1 = data.split(",")
+    tuple1 = tuple(list1)
+    print("List version: ",list1)
+    print("Tuple version: ",tuple1)
+
+#commenting out so it doesn't prompt every time
+#comma_separated()
+
+#7: Write a Python program to accept a filename from the user and print the extension of that.
+def file_extension ():
+    filename = input("Please enter the full filename: ")
+    etc, extension = os.path.splitext (filename)
+    print(extension)
+
+#commenting out so it doesn't prompt every time
+#file_extension()
+
+#8: Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+
+print(color_list[0],color_list[-1])
+
+#9: Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+exam_st_date = (11, 12, 2014)
+
+print("The examination will start from: %i/%i/%i"%exam_st_date)
+
+#10: Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+def raised_twice ():
+    value = input("Please input an integer: ")
+    calculation = int(value) + int(value*2) + int(value*3)
+    print(calculation)
+
+#commenting out so it doesn't prompt every time
+#raised_twice()
